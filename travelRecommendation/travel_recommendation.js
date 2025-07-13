f26a291d4b1a90dffdea4c75fd7780b3c1222f76
pawan dubey <pvndb2015@gmail.com>
2025-07-13 16:19:34 +0530

    changes done in api and upload images

--- travelRecommendation/travel_recommendation.js
+++ travelRecommendation/travel_recommendation.js
@@ -35,7 +35,7 @@ document.getElementById('searchButton').addEventListener('click', function () {
           });
         }
       });
-      
+
 
       // Search in temples
       data.temples.forEach(temple => {
@@ -88,3 +88,7 @@ function displayResults(results) {
     container.appendChild(div);
   });
 }
+
+document.querySelector('.clear-btn').addEventListener('click', function () {
+  document.getElementById('searchInput').value = '';
+});
